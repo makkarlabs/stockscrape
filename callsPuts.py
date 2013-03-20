87bc0f9f93050fcdf8172d31d2ebb6d515b2a0be
realslimkarthik <karthikhariharan13@gmail.com>
2013-03-21 00:42:23 +0530

    Finished

--- callsPuts.py
+++ callsPuts.py
@@ -23,38 +23,40 @@ def fetch(params):
 		putsObj = {}
 		strikePrice = 0
 		counter = 0
+		table = { ord(u'-'): None}
 
-		callsObj['OI'] = cellData[0].get_text()
-		putsObj['OI'] = cellData[16].get_text()
+		callsObj['OI'] = cellData[0].get_text().translate(table)
+		putsObj['OI'] = cellData[16].get_text().translate(table)
 
-		callsObj['Volume'] = cellData[1].get_text()
-		putsObj['Volume'] = cellData[15].get_text()
+		callsObj['Volume'] = cellData[1].get_text().translate(table)
+		putsObj['Volume'] = cellData[15].get_text().translate(table)
 
-		callsObj['IV'] = cellData[2].get_text()
-		putsObj['IV'] = cellData[14].get_text()
+		callsObj['IV'] = cellData[2].get_text().translate(table)
+		putsObj['IV'] = cellData[14].get_text().translate(table)
 
 		temp = cellData[3].get_text()
 		ind1 = temp.find('>', 0, len(temp))
 		ind2 = temp.find('<', ind1+1, len(temp))
 		ltp = temp[ind1+2:ind2]
-		callsObj['LTP'] = ltp
+		callsObj['LTP'] = ltp.translate(table)
+
 		temp = cellData[13].get_text()
 		ind1 = temp.find('>', 0, len(temp))
 		ind2 = temp.find('<', ind1+1, len(temp))
 		ltp = temp[ind1+2:ind2]
-		putsObj['LTP'] = ltp
+		putsObj['LTP'] = ltp.translate(table)
 		
-		callsObj['BidQty'] = cellData[4].get_text()
-		putsObj['BidQty'] = cellData[9].get_text()
+		callsObj['BidQty'] = cellData[4].get_text().translate(table)
+		putsObj['BidQty'] = cellData[9].get_text().translate(table)
 
-		callsObj['BidPrice'] = cellData[5].get_text()
-		putsObj['BidPrice'] = cellData[10].get_text()
+		callsObj['BidPrice'] = cellData[5].get_text().translate(table)
+		putsObj['BidPrice'] = cellData[10].get_text().translate(table)
 
-		callsObj['AskQty'] = cellData[7].get_text()
-		putsObj['AskQty'] = cellData[12].get_text()
+		callsObj['AskQty'] = cellData[7].get_text().translate(table)
+		putsObj['AskQty'] = cellData[12].get_text().translate(table)
 
-		callsObj['AskPrice'] = cellData[6].get_text()
-		putsObj['AskPrice'] = cellData[11].get_text()
+		callsObj['AskPrice'] = cellData[6].get_text().translate(table)
+		putsObj['AskPrice'] = cellData[11].get_text().translate(table)
 
 		temp = cellData[8].get_text()
 		ind1 = temp.find('a0', 0, len(temp))
