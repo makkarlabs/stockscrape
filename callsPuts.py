02f089412d9dfa91fb40b5084cb7b9f5f745b290
Karthik <realslimkarthik@realslimkarthik-Inspiron-N4010.(none)>
2013-03-20 19:19:10 +0530

    json obtained

--- callsPuts.py
+++ callsPuts.py
@@ -8,27 +8,43 @@ html = get_data('http://www.nseindia.com/live_market/dynaContent/live_watch/fxTr
 opttbl = SoupStrainer('div', {'class': 'opttbldata'})
 data = BeautifulSoup(html, 'html.parser', parse_only=opttbl)
 tbldata = data.find_all('tr')
+tbldata.pop(0)
+tbldata.pop(0)
 jsonObj = []
 
 for i in tbldata:
-	cellData = i.find_all('td')
-	callsObj = []
-	putsObj = []
+	cellData = i.find_all('td', align='right')
+	callsObj = {}
+	putsObj = {}
 	strikePrice = 0
 	counter = 0
-	#print cellData
-	for j in cellData:
-		if counter == 0:
-			counter + 1
-			continue
-		elif counter < 10:
-			putsObj.append(j.get_text())
-		elif counter == 10:
-			strikePrice = j.get_text()
-		elif counter > 10:
-			callsObj.append(j.get_text())
-		else:
-			print json.dumps({"puts": putsObj, "calls": callsObj})
-		counter = counter + 1
-		print counter
-		print j.get_text()
+
+	callsObj['OI'] = cellData[0].get_text()
+	putsObj['OI'] = cellData[16].get_text()
+
+	callsObj['Volume'] = cellData[1].get_text()
+	putsObj['Volume'] = cellData[15].get_text()
+
+	callsObj['IV'] = cellData[2].get_text()
+	putsObj['IV'] = cellData[14].get_text()
+
+	callsObj['LTP'] = cellData[3].get_text()
+	putsObj['LTP'] = cellData[13].get_text()
+
+	callsObj['BidQty'] = cellData[4].get_text()
+	putsObj['BidQty'] = cellData[9].get_text()
+
+	callsObj['BidPrice'] = cellData[5].get_text()
+	putsObj['BidPrice'] = cellData[10].get_text()
+
+	callsObj['AskQty'] = cellData[7].get_text()
+	putsObj['AskQty'] = cellData[12].get_text()
+
+	callsObj['AskPrice'] = cellData[6].get_text()
+	putsObj['AskPrice'] = cellData[11].get_text()
+
+	strikePrice = cellData[8].get_text()
+
+	jsonObj.append(json.dumps({'StrikePrice': strikePrice, 'Calls': callsObj, 'Puts': putsObj}))
+
+print jsonObj
